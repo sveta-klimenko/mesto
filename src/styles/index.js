4ddeceb66e9062faf5778dbcc42e06725be978e9
Klimenko Sveta <rosedogknight@yandex.ru>
2022-05-13 19:49:35 +0300

    added WebPack

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,12 +1,12 @@
-import Card from "./Card.js";
-import Popup from "./Popup.js";
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from "./PopupWithImage.js";
-import FormValidator from "./FormValidator.js";
-import { initialCards, validationList } from "./initial-cards.js";
-import Section from "./Section.js";
-import UserInfo from "./UserInfo.js";
+import Card from "../components/Card.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import FormValidator from "../components/FormValidator.js";
+import { initialCards, validationList } from "../components/initial-cards.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 // import { openPopup, popupShowPicture, closePopup } from "./utils.js";
+import "./index.css";
 
 //попапы
 const popupPersonal = document.querySelector(".popup_personal-info");
